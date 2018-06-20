8578406ee0b640a8b7969bc7e9c4d1c4cfac5aac
albinvinoy <albinvinoy@gmail.com>
2018-06-19 23:35:29 -0700

    updated yelp need to work on front end

--- Exploree/accessKey.py
+++
@@ -1,2 +0,0 @@
-YELP_API_KEY="_"
-GEO_ACCESS_KEY = "" #api.ipstack.com
--- Exploree/views.py
+++ Exploree/views.py
@@ -3,29 +3,72 @@ import requests
 from .accessKey import GEO_ACCESS_KEY, YELP_API_KEY
 # Create your views here.
 
-def index(request):
-    print(getGeoCoordinates())
+#main - make request
+def makeRequest(url, params=None, headers=None):
+    response = requests.get(url, headers=headers, params=params)
+    return response
 
-    return render(request, 'exploree/index.html')
 
 
-def food(request):
-    return HttpResponse("This is food page")
-
-def activitites(request):
-    return HttpResponse("This is the activities page")
-    
 # access the ipstack to get geo location
 def getGeoCoordinates():
-    baseUrl = "http://api.ipstack.com/check?access_key={}&security=1&fields=main".format(GEO_ACCESS_KEY)
-    response = requests.get(baseUrl)
+    baseUrl = "http://api.ipstack.com/check?"
+
+    url_params = {
+        "access_key" : GEO_ACCESS_KEY,
+        "security" : 1,
+        "fields" : "main"
+    }
+    # baseUrl = "http://api.ipstack.com/check?access_key={}&security=1&fields=main".format(GEO_ACCESS_KEY)
+    response = makeRequest(baseUrl, params=url_params)
     if response.status_code == 200:
         data = response.json()
         return [data["latitude"], data["longitude"]]
     else:
-        print("Failed to get response")
-    return [34.0522, 118.2437] #default to LA
+        # print("Failed to get response")
+        return [34.0522, 118.2437] #default to LA
+
+
 
 #yelp api
-def requestYelpData(url, searchTerms, urlParams):
-    pass
+
+def getYelpInfo(keyword="", coordinates=[]):
+    term = keyword or "dinner" # get from form
+    SEARCH_LIMIT = 1 # 10 as default
+    url = "https://api.yelp.com/v3/businesses/search"
+
+    headers = {
+        'Authorization': 'Bearer %s' % YELP_API_KEY,
+    }
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'latitude': coordinates[0],
+        'longitude' : coordinates[1],
+        'limit': SEARCH_LIMIT
+    }
+
+    response = makeRequest(url=url, params=url_params, headers=headers)
+    return response
+
+
+
+
+#main view functions
+def index(request):
+    return render(request, 'exploree/index.html')
+
+
+def food(request):
+    r = getYelpInfo('dinner', getGeoCoordinates())
+    if r.status_code==200:
+        return render(request, 'exploree/food.html', context={'raw_data':r.json()})
+    else:
+        return HttpResponse("Failed to Load Data {}".format(r))
+
+def activitites(request):
+    r = getYelpInfo('activities', getGeoCoordinates())
+    if r.status_code==200:
+        return render(request, 'exploree/activities.html', context={'raw_data':r.json()})
+    else:
+        return HttpResponse("Failed to Load Data {}".format(r))
